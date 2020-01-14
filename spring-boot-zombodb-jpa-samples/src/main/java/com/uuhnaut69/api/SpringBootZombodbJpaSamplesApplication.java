dee937725f9050ebbb68a48911c2225f37d495a6
uuhnaut69 <uuhnaut96@gmail.com>
2020-01-14 20:54:21 +0700

    # Init zombodb integrate with spring boot jpa, test some features

---
+++ spring-boot-zombodb-jpa-samples/src/main/java/com/uuhnaut69/api/SpringBootZombodbJpaSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.uuhnaut69.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZombodbJpaSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZombodbJpaSamplesApplication.class, args);
+	}
+
+}
